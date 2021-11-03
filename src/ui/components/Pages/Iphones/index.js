938cd3749dd65210b95684e67b366bf273b13442
raphael <raphaelsantosdesigner@gmail.com>
2021-11-02 21:27:36 -0700

    fetch data with axios

--- src/ui/components/Pages/Iphones/index.js
+++ src/ui/components/Pages/Iphones/index.js
@@ -5,73 +5,62 @@ import {useState, useEffect} from 'react';
 import Box from '@tds/core-box';
 import Devices from '../../Devices';
 import Spinner from '@tds/core-spinner';
-import Image from '../../../../../public/images/iphone.jpg'
+import Image from '../../../../../public/images/iphone.jpg';
+import axios from "axios";
+import regeneratorRuntime from "regenerator-runtime";    
 
 const Iphones = () => {
     const [isFetching,setIsFetching] = useState(false);
     const [iphones, setIphones] = useState([]);
 
     useEffect(() => {
-        const getIphones =  () => {
-            const url = "http://localhost:8081/iphones/";
-            let headers = {}
-            setIsFetching(true);
-            fetch(url,{
-                method : "GET",
-                mode: "cors",
-                headers: headers
-            })
-            .then((res) => {
-                if(!res.ok) {
-                    throw new Error(response.error)
-                }
-                return res.json();
-            })
-            .then(data => {
-                setIphones(data.data);
+        const getIphones = async () => {
+            try {
+                setIsFetching(true);
+                const listIphones = await axios.get(`http://localhost:8081/iphones`);
+                setIphones(listIphones.data.data);
                 setIsFetching(false);
-            })
-            .catch((error)=> {
-                console.log(error)
-            })
+            } catch (error) {
+                console.log(error);
+            }
         };
         getIphones();
-  }, []);
-  if (isFetching) {
-    return (
-        <FlexGrid>
-            <FlexGrid.Col>
-                <FlexGrid.Row verticalAlign="middle" horizontalAlign="center">
-                <Box vertical={2}>
-                    <div style={{ height: 100 }} />
-                    <Spinner
-                        spinning
-                        label={
-                            <>
-                            Loading 
-                            </>
-                        } 
-                    />
-                </Box>
-                </FlexGrid.Row>
-            </FlexGrid.Col>
-        </FlexGrid>
-      )
-  }
-  return(
-      <>
-        <FlexGrid>
-            <FlexGrid.Col>
+    }, []);
+    if (isFetching) {
+        return (
+            <FlexGrid>
+                <FlexGrid.Col>
+                    <FlexGrid.Row verticalAlign="middle" horizontalAlign="center">
+                    <Box vertical={2}>
+                        <div style={{ height: 100 }} />
+                        <Spinner
+                            spinning
+                            label={
+                                <>
+                                Loading 
+                                </>
+                            } 
+                        />
+                    </Box>
+                    </FlexGrid.Row>
+                </FlexGrid.Col>
+            </FlexGrid>
+        )
+    }
+    return(
+        <>
+            <FlexGrid>
+                <FlexGrid.Col>
+                    <FlexGrid.Row>
+                        <Heading level="h1">Iphones</Heading>
+                    </FlexGrid.Row>
+                </FlexGrid.Col>
                 <FlexGrid.Row>
-                    <Heading level="h1">Iphones</Heading>
+                    <Devices devices={iphones} image={Image} type="iphone" />
                 </FlexGrid.Row>
-            </FlexGrid.Col>
-            <FlexGrid.Row>
-                <Devices devices={iphones} image={Image} type="iphone" />
-            </FlexGrid.Row>
-        </FlexGrid>
-      </>
-  )
-};
+            </FlexGrid>
+        </>
+    )
+    };
 
 export default Iphones;
--- src/ui/components/Pages/Watches/index.js
+++ src/ui/components/Pages/Watches/index.js
@@ -5,70 +5,59 @@ import Box from '@tds/core-box';
 import Devices from '../../Devices';
 import Spinner from '@tds/core-spinner';
 import Image from '../../../../../public/images/watch.jpg';
+import axios from "axios";
+import regeneratorRuntime from "regenerator-runtime";
 
 const Watches = () => {
     const [watches, setWatches] = useState([]);
     const [isFetching,setIsFetching] = useState(false);
-
+    
     useEffect(() => {
-        const getWatches =  () => {
-            const url = "http://localhost:8081/watches/";
-            let headers = {}
+        const getWatches = async () => {
+          try {
             setIsFetching(true);
-            fetch(url,{
-                method : "GET",
-                mode: "cors",
-                headers: headers
-            })
-            .then((res) => {
-                if(!res.ok) {
-                    throw new Error(response.error)
-                }
-                return res.json();
-            })
-            .then(data => {
-                setWatches(data.data);
-                setIsFetching(false);
-            })
-            .catch((error)=> {
-                console.log(error)
-            })
+            const listWatches = await axios.get(`http://localhost:8081/watches`);
+            setWatches(listWatches.data.data);
+            setIsFetching(false);
+          } catch (error) {
+            console.log(error);
+          }
         };
         getWatches();
-    }, []);
-    if (isFetching) {
-        return (
+      }, []);
+        if (isFetching) {
+            return (
+                <FlexGrid>
+                    <FlexGrid.Col>
+                        <FlexGrid.Row verticalAlign="middle" horizontalAlign="center">
+                            <Box vertical={2}>
+                                <div style={{ height: 100 }} />
+                                <Spinner
+                                    spinning
+                                    label={
+                                        <>
+                                        Loading 
+                                        </>
+                                    } 
+                                />
+                            </Box>
+                        </FlexGrid.Row>
+                    </FlexGrid.Col>
+                </FlexGrid>
+                )
+            }
+            return(
             <FlexGrid>
                 <FlexGrid.Col>
-                    <FlexGrid.Row verticalAlign="middle" horizontalAlign="center">
-                        <Box vertical={2}>
-                            <div style={{ height: 100 }} />
-                            <Spinner
-                                spinning
-                                label={
-                                    <>
-                                    Loading 
-                                    </>
-                                } 
-                            />
-                        </Box>
+                    <FlexGrid.Row>
+                        <Heading level="h1">Watches</Heading>
                     </FlexGrid.Row>
                 </FlexGrid.Col>
+                <FlexGrid.Row>
+                <Devices devices={watches} image={Image} type="watch"  />
+                </FlexGrid.Row>
             </FlexGrid>
             )
-        }
-        return(
-        <FlexGrid>
-            <FlexGrid.Col>
-                <FlexGrid.Row>
-                    <Heading level="h1">Watches</Heading>
-                </FlexGrid.Row>
-            </FlexGrid.Col>
-            <FlexGrid.Row>
-            <Devices devices={watches} image={Image} type="watch"  />
-            </FlexGrid.Row>
-        </FlexGrid>
-        )
     };
 
     export default Watches;
